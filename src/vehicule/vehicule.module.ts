70387d6471681fad99f061d071bb15727ddeefaa
Niortien <Niortiencoulibaly2001@gmail.com>
2025-12-29 09:13:53 +0000

    prisma2

--- src/vehicule/vehicule.module.ts
+++ src/vehicule/vehicule.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { VehiculeService } from './vehicule.service';
 import { VehiculeController } from './vehicule.controller';
-import { DatabaseModule } from 'src/database/database.module';
+import { DatabaseModule } from '../database/database.module';
 
 @Module({
   imports: [DatabaseModule],
